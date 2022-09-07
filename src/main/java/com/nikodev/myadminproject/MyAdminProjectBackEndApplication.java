7da860f0f2a1af8ed2f23a73af79d39315dc87da
NIAMA-KOUMBA <niamakoumban@who.int>
2022-09-07 16:56:21 +0100

    Creation of spring boot app for the back end

---
+++ src/main/java/com/nikodev/myadminproject/MyAdminProjectBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.nikodev.myadminproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAdminProjectBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyAdminProjectBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nikodev/myadminproject/MyAdminProjectBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikodev.myadminproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAdminProjectBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
